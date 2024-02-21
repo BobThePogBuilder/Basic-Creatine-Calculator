fd5065777023297ba91e15272c45a810ea4d8117
BobThePogBuilder <ssellabahmadzai@gmail.com>
2024-02-20 22:54:10 -0800

    Updated and more accurate

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        /* Enables the ablility to get user input */
+        /* Enables the ability to get user input */
         Scanner scnr = new Scanner(System.in);
         //initializing our values
         int weight;
-        double creatine;
         // Prints text for the user
         System.out.print("Please Enter your weight: ");
         //Use to calculate weight and the .014 g per Pound of body weight
         /* three to five grams per day 14 mg per pound of body weight  */
         weight = scnr.nextInt();
-        creatine = .014 * weight;
 
 
-
-        System.out.println("Your weight is " + weight + " LBs." + " We recommend you take a minimum of " + creatine + " gram of Creatine Daily");
 
         /*
         Under 120 pounds: 3 grams
@@ -25,12 +21,14 @@ public class Main {
          */
 
         if (weight <= 120) {
-            System.out.println("The recommended dose is 3 Grams Daily");
+            System.out.println("You are " + weight + " lbs." + " The recommended dose is 3 Grams Daily for people 120 pounds or under.");
         }
-        if (weight > 120) {
-            System.out.println("The recommended dose is 5 Grams Daily");
+        else if (weight <= 200) {
+            System.out.println("You are " + weight + " lbs." + " The recommended dose is 5 Grams Daily for people 121 pounds to 200 pounds.");
         }
-
+        else {
+            System.out.println("You are " + weight + " lbs." + " The recommended dose is 8 Grams Daily for people 200 pounds and over.");
+        }
         System.out.println("Don't Forget To Drink Lots Of Water When Taking Creatine!!!!");
 
 
